16b65c4fe01bb929d4ebca030c3908df34cfe650
Rexford <rex982@live.com>
2018-11-01 01:02:38 -0400

    modying

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import App from './App';
+import Hello from "./components/Hello";
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById('root'));
+//vendors
+import "tachyons";
+// import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<Hello greeting={"Hello " + "React Ninja"}/>, document.getElementById('root'));
+
+
+
+
+
+
+
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
